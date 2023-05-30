b78c59c27a940ac5e7dceb0e0bef0360e2aaf7e1
simonwardle <simon.wardle@outlook.com>
2023-05-30 22:24:54 +0100

    Lect 62 return JSON

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/simonwardle/bookings/pkg/config"
@@ -43,7 +45,7 @@ func (rec *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap["test"] = "Hello again!"
 
 	remoteIP := rec.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP 
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, r, "about.tmpl.html", &models.TemplateData{
 		StringMap: stringMap,
@@ -52,20 +54,17 @@ func (rec *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Generals is the generals page handler
 func (rec *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "generals.tmpl.html", &models.TemplateData{
-	})
+	render.RenderTemplate(w, r, "generals.tmpl.html", &models.TemplateData{})
 }
 
 // Majors is the majorss page handler
 func (rec *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "majors.tmpl.html", &models.TemplateData{
-	})
+	render.RenderTemplate(w, r, "majors.tmpl.html", &models.TemplateData{})
 }
 
 // Availability search for room availability
 func (rec *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "search-availability.tmpl.html", &models.TemplateData{
-	})
+	render.RenderTemplate(w, r, "search-availability.tmpl.html", &models.TemplateData{})
 }
 
 // PostAvailability search for room availability
@@ -75,14 +74,34 @@ func (rec *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) 
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s.", start, end)))
 }
 
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availablity and send JSON reponse
+func (rec *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
 // Contact is the handler for the contact page
 func (rec *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "contact.tmpl.html", &models.TemplateData{
-	})
+	render.RenderTemplate(w, r, "contact.tmpl.html", &models.TemplateData{})
 }
 
 // Reservation capture client details
 func (rec *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r,"make-reservation.tmpl.html", &models.TemplateData{
-	})
+	render.RenderTemplate(w, r, "make-reservation.tmpl.html", &models.TemplateData{})
 }
